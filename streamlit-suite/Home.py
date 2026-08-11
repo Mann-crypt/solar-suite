27816c2435395060a7be2e56760cfe8e8bdb894d
Manjot Singh <manjotsingh2004sm@gmail.com>
2026-08-11 14:46:26 +0530

    Update Home.py

--- streamlit-suite/Home.py
+++ streamlit-suite/Home.py
@@ -118,7 +118,7 @@ with col1:
         key="home_loss_correction",
         use_container_width=True,
     ):
-        st.switch_page("pages/1_🌤️_Loss_Correction.py")
+        st.switch_page("pages/1_Loss_Correction.py")
 
 
 # ==========================================================
@@ -145,7 +145,7 @@ with col2:
         key="home_rt_correction",
         use_container_width=True,
     ):
-        st.switch_page("pages/2_⏰_RT_Correction.py")
+        st.switch_page("pages/2_RT_Correction.py")
 
 
 # ==========================================================
@@ -172,4 +172,4 @@ with col3:
         key="home_aeromal",
         use_container_width=True,
     ):
-        st.switch_page("pages/3_🐱‍🏍_Aeromal.py")
+        st.switch_page("pages/3_Aeromal.py")
