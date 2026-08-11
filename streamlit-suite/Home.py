1ec1b035ae238c1705b86ade22165d178dee6516
Manjot Singh <manjotsingh2004sm@gmail.com>
2026-08-11 15:14:04 +0530

    Update Home.py

--- streamlit-suite/Home.py
+++ streamlit-suite/Home.py
@@ -26,7 +26,7 @@ if st.session_state.get("aeromal_auth", False):
 
 st.sidebar.markdown("""
 <div style='text-align:center;color: gray;font-size:13px;line-height:2;'>
-Developed and Maintained by:<br><b style='color:#4A5568;'>Manjot Singh</b><br>
+Developed and Maintained by:<br><b style='color:#CBD5E1;'>Manjot Singh</b><br>
 Script Writer:<br><b style='color:gray;'>Tushar Sharma</b><br>
 Challenger:<br><b style='color:gray;'>Aarav Sharma</b><br>
 Tester:<br><b style='color:gray;'>Jatin Chaturvedi</b><br>
