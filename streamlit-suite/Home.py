2bcba25f0428bd2a6e1be12a6f14f7488b78646e
Manjot Singh <manjotsingh2004sm@gmail.com>
2026-08-11 14:53:04 +0530

    Update Home.py

--- streamlit-suite/Home.py
+++ streamlit-suite/Home.py
@@ -22,7 +22,7 @@ if st.session_state.get("aeromal_auth", False):
     if st.sidebar.button("🚪 Logout Aeromal", use_container_width=True):
         st.session_state.aeromal_auth = False
         st.rerun()
-        st.sidebar.divider()
+    st.sidebar.divider()
 
 st.sidebar.markdown("""
 <div style='text-align:center;color:gray;font-size:13px;line-height:2;'>
